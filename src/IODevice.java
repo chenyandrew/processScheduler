01d7cb1daba5d813cbff313ed4eaae149b887afa
frankSaldivar <franksaldivar76@gmail.com>
2018-10-08 16:57:05 -0500

    Filled out bubblesort() in IO and CPU
    Added function to generate 100k random ints, use that functions in
    bubblesort and then run bubblesort on random list

--- src/IODevice.java
+++ src/IODevice.java
@@ -18,8 +18,36 @@ public class IODevice {
         return "ready";
     }
 
+  //generates a random list of size n, runs bubble sort O(n^2)
     public void BubbleSort(){
+    	
+    	List<Integer> listToSort = rng(100000);
+    	
+    	for (int i = 0; i < listToSort.size()-1; ++i)
+    	{
+    		for (int j = 0; j < listToSort.size()-1; ++j)
+    		{
+    			if (listToSort.indexOf(j) > listToSort.indexOf(j+1))
+    			{
+    				Collections.swap(listToSort, j, j+1);
+    			}
+    		}
+    	}
+    	
 
+    }
+    public List<Integer> rng(int n)
+    {
+        List<Integer> result = new ArrayList<>();
+        double rand = 0;
+        for (int i : result)
+        {
+            rand = (Math.random() * 100000)+1;
+            result.add((int) rand);
+
+        }//looping until n-many random ints btw 1-100k into list
+
+        return result;
     }
 
 }
